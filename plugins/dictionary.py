f00e73ed1924f7f9666adf8713587ce93e6cfeaf
shane <haroboy876@gmail.com>
2017-05-13 12:00:32 -0700

    remove ninjawords lookup, refer to urban dictionary

--- plugins/dictionary.py
+++ plugins/dictionary.py
@@ -27,61 +27,8 @@ def urban(inp):
 @hook.command('dictionary')
 @hook.command
 def define(inp):
-    ".define/.dictionary <word> -- fetches definition of <word>"
 
-    url = 'http://ninjawords.com/'
-
-    h = http.get_html(url + http.quote_plus(inp))
-
-    definition = h.xpath('//dd[@class="article"] | '
-                         '//div[@class="definition"] |'
-                         '//div[@class="example"]')
-
-    if not definition:
-        return 'No results for ' + inp
-
-    def format_output(show_examples):
-        result = '%s: ' % h.xpath('//dt[@class="title-word"]/a/text()')[0]
-
-        correction = h.xpath('//span[@class="correct-word"]/text()')
-        if correction:
-            result = 'definition for "%s": ' % correction[0]
-
-        sections = []
-        for section in definition:
-            if section.attrib['class'] == 'article':
-                sections += [[section.text_content() + ': ']]
-            elif section.attrib['class'] == 'example':
-                if show_examples:
-                    sections[-1][-1] += ' ' + section.text_content()
-            else:
-                sections[-1] += [section.text_content()]
-
-        for article in sections:
-            result += article[0]
-            if len(article) > 2:
-                result += ' '.join('%d. %s' % (n + 1, section)
-                                   for n, section in enumerate(article[1:]))
-            else:
-                result += article[1] + ' '
-
-        synonyms = h.xpath('//dd[@class="synonyms"]')
-        if synonyms:
-            result += synonyms[0].text_content()
-
-        result = re.sub(r'\s+', ' ', result)
-        result = re.sub('\xb0', '', result)
-        return result
-
-    result = format_output(True)
-    if len(result) > 450:
-        result = format_output(False)
-
-    if len(result) > 450:
-        result = result[:result.rfind(' ', 0, 450)]
-        result = re.sub(r'[^A-Za-z]+\.?$', '', result) + ' ...'
-
-    return result
+    return urban(inp)
 
 
 @hook.command('e')
